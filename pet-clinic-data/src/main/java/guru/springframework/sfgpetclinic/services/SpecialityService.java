1c7afd11bb89faf375c25bee1ee12547638bef48
Roberto Benitez Monje <roberto.benitez.es@gmail.com>
2018-11-10 22:45:00 +0100

    added Speciality Service Map Impl . Closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,5 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {    }
