388c9216ffce92fe05d81ae3495e09e425a9762f
Roberto Benitez Monje <roberto.benitez.es@gmail.com>
2018-11-11 13:53:10 +0100

    Added project Lombok and refactored. Closes #51

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -1,11 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import java.time.LocalDate;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Visit extends BaseEntity {
 
@@ -18,28 +25,4 @@ public class Visit extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "pet_id")
     private Pet pet;
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
 }
