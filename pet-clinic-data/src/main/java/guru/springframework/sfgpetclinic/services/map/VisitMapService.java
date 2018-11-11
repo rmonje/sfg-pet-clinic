f207c88db3b409da4e7503640efd8a46b6d8213a
Roberto Benitez Monje <roberto.benitez.es@gmail.com>
2018-11-11 12:28:42 +0100

    Implemented Map impl for Visit Service. Closes #46

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
     @Override
     public Set<Visit> findAll() {
         return super.findAll();
@@ -19,8 +19,12 @@ public class VisitServiceMap extends AbstractMapService<Visit, Long> implements 
     }
 
     @Override
-    public Visit save(Visit object) {
-        return super.save(object);
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null ||
+        visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
     }
 
     @Override
